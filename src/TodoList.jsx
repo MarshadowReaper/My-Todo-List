c9270c549d66665007959ea096e4cbbfb5727d81
Tajee <tajeedobbins16@gmail.com>
2026-04-10 11:40:23 -0400

    useState and TodoListItem

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,16 +1,15 @@
-const todoList = [
-  { id: 1, title: "review resources" },
-  { id: 2, title: "take notes" },
-  { id: 3, title: "code out app" },
-  { id: 4, title: "take quizes" },
-];
-function TodoList() {
+import TodoListItem from "./TodoListItem.jsx";
+function TodoList({ todoList }) {
+  // recieves data
   return (
     <ul>
       {todoList.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
+        // Will loop through the data
+        <TodoListItem todo={todo} key={todo.id} />
       ))}
     </ul>
+    //(<TodoListItem todo={todo} key={todo.id}/>)))} Is how the data gets Rendered
   );
 }
+
 export default TodoList;
