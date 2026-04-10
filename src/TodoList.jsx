eb0858be9e2ef3a758024568deba70a215ed8fce
Tajee <tajeedobbins16@gmail.com>
2026-04-10 16:56:56 -0400

    Set up complete

---
+++ src/TodoList.jsx
@@ -0,0 +1,15 @@
+import TodoListItem from "./TodoListItem.jsx";
+function TodoList({ todoList }) {
+  // recieves data
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        // Will loop through the data
+        <TodoListItem todo={todo} key={todo.id} />
+      ))}
+    </ul>
+    //(<TodoListItem todo={todo} key={todo.id}/>)))} Is how the data gets Rendered
+  );
+}
+
+export default TodoList;
