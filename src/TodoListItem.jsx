fecb6342904248aa2864f145d8f53b7954c750d3
Tajee <tajeedobbins16@gmail.com>
2026-04-16 11:43:31 -0400

    Forgotten information

---
+++ src/TodoListItem.jsx
@@ -0,0 +1,9 @@
+function TodoListItem({ todo }) {
+  //This Recieves one item
+  return (
+    <li>{todo.title}</li>
+
+    // and this is how it gets displayed
+  );
+}
+export default TodoListItem;
