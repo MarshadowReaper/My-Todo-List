566e634e2bd7825a8bdd3d152f50e94699358b4f
Tajee <tajeedobbins16@gmail.com>
2026-04-23 13:07:05 -0400

    temparary checkpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import TodoForm from "./TodoForm.jsx";
 import TodoList from "./TodoList.jsx";
+import { useState } from "react";
+
+//This is holding the data
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  //This is how to create a state
+  function addTodo(todoTitle) {
+    const toDo = { id: Date.now(), title: todoTitle };
+
+    setTodoList((previous) => [newTodo, ...previous]);
+  }
+
   return (
     <div>
       <h1> My Todos </h1>
-      <TodoForm />
-      <TodoList />
+      <TodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
-
+//<TodoList todoList={todoList} /> This is managing and passing data down to children
 export default App;
+
+//Programmer's note: I had a LOT of troubleshooting to do with this one so figured to help understand the lesson better I should label what role the codes are playing
