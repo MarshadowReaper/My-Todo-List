b58a5a8b181f2cbacb636263074d6ddbf0022cab
Tajee <tajeedobbins16@gmail.com>
2026-04-27 11:15:06 -0400

    small fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import TodoForm from "./TodoForm.jsx";
 import TodoList from "./TodoList.jsx";
 import { useState } from "react";
-
+const todos = [
+  { id: 1, title: "review resources" },
+  { id: 2, title: "take notes" },
+  { id: 3, title: "code out app" },
+  { id: 4, title: "take quizes" },
+];
 function App() {
   const [todoList, setTodoList] = useState(todos);
   //This is how to create a state
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,25 +1,8 @@
 import TodoListItem from "./TodoListItem.jsx";
-const todos = [
-  { id: 1, title: "review resources" },
-  { id: 2, title: "take notes" },
-  { id: 3, title: "code out app" },
-  { id: 4, title: "take quizes" },
-];
+
 //This is holding the data
 function TodoList({ todoList }) {
   // recieves data
-  return (
-    <ul>
-      {todoList.map((todo) => (
-        // Will loop through the data
-        <TodoListItem todo={todo} key={todo.id} />
-      ))}
-    </ul>
-    //(<TodoListItem todo={todo} key={todo.id}/>)))} Is how the data gets Rendered
-  );
-}
-
-function TodoList() {
   return (
     <ul>
       {todoList.map((todo) => (
