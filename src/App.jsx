f6c2746117e38d4ca6f3776c2e1d3fe429a3f1eb
Tajee <tajeedobbins16@gmail.com>
2026-05-12 23:11:37 -0400

    Forgotten update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import TodoForm from "./TodoForm.jsx";
-import TodoList from "./TodoList.jsx";
+import TodoForm from "./features/TodoForm.jsx";
+import TodoList from "./features/TodoList/TodoList.jsx";
 import { useState } from "react";
 
 //This is holding the data
@@ -22,12 +22,26 @@ function App() {
     });
     setTodoList(updatedList);
   }
+  const updateTodo = (editedTodo) => {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === editedTodo.id) {
+        return { ...editedTodo };
+      }
+      return todo;
+    });
+
+    setTodoList(updatedTodos);
+  };
 
   return (
     <div>
       <h1> My Todos </h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
+      <TodoList
+        todoList={todoList}
+        onCompleteTodo={completeTodo}
+        onUpdateTodo={updateTodo}
+      />
     </div>
   );
 }
