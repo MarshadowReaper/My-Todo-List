db0a35a7cce027bd822f9d835efddee8870a98ff
Tajee <tajeedobbins16@gmail.com>
2026-05-26 13:12:31 -0400

    Undoing reviewer mistake + ACTUAL ISSUE FIX

--- src/features/Todos/TodoList/TodoList.jsx
+++ src/features/Todos/TodoList/TodoList.jsx
@@ -2,12 +2,9 @@ import TodoListItem from "./TodoListItem.jsx";
 import { useMemo } from "react";
 function TodoList({ todoList, onCompleteTodo, onUpdateTodo, dataVersion }) {
   const filteredTodoList = useMemo(() => {
-    const activeTodos = todoList.filter((todo) => !todo.isCompleted);
+    console.log(`Recalculating filtered todos (v${dataVersion})`);
 
-    return {
-      version: dataVersion,
-      todos: activeTodos,
-    };
+    return todoList.filter((todo) => !todo.isCompleted);
   }, [todoList, dataVersion]);
   const clear = filteredTodoList.todos.length === 0;
 
