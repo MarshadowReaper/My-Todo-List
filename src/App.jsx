d43565726388a5a6eb809e2f94d79126872f2ef1
Tajee <tajeedobbins16@gmail.com>
2026-06-04 15:41:26 -0400

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
 import TodoForm from "./TodoForm.jsx";
 import TodoList from "./TodoList.jsx";
 import { useState } from "react";
-<<<<<<< HEAD
 
 //This is holding the data
 //A small hello to the instructor this part was skipped by git
-=======
-const todos = [
-  { id: 1, title: "review resources" },
-  { id: 2, title: "take notes" },
-  { id: 3, title: "code out app" },
-  { id: 4, title: "take quizes" },
-];
->>>>>>> 5ccf78cdd78450099556d658f091c881979cc613
 function App() {
-  const [todoList, setTodoList] = useState(todos);
+  const [todoList, setTodoList] = useState([]);
   //This is how to create a state
+  function addTodo(todoTitle) {
+    const newTodo = { id: Date.now(), title: todoTitle };
+
+    setTodoList((previous) => [newTodo, ...previous]);
+  }
+
   return (
     <div>
       <h1> My Todos </h1>
-      <TodoForm />
+      <TodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
     </div>
   );
 }
 //<TodoList todoList={todoList} /> This is managing and passing data down to children
 export default App;
-
-//Programmer's note: I had a LOT of troubleshooting to do with this one so figured to help understand the lesson better I should label what role the codes are playing
