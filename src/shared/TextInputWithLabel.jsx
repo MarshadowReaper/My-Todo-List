e28b13cb4f99bab0d8f8142c718235876f3a178f
Tajee <tajeedobbins16@gmail.com>
2026-06-07 12:18:23 -0400

    no change...?

--- src/shared/TextInputWithLabel.jsx
+++ src/shared/TextInputWithLabel.jsx
@@ -1,10 +1,4 @@
-function TextInputWithLabel({
-  labelText,
-  elementId,
-  onChange,
-  value,
-  inputRef,
-}) {
+function TextInputWithLabel({ labelText, elementId, onChange, value, ref }) {
   return (
     <>
       <label htmlFor={elementId}>{labelText}</label>
@@ -14,7 +8,7 @@ function TextInputWithLabel({
         id={elementId}
         value={value}
         onChange={onChange}
-        ref={inputRef}
+        ref={ref}
       />
     </>
   );
