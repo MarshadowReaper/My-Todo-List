61662d745d821b99f62140d0f15a559fc7c94761
Tajee Goode <tajeedobbins16@gmail.com>
2026-06-13 10:13:23 -0400

    skipped update

--- src/features/Logon.jsx
+++ src/features/Logon.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAuth } from "../context/AuthContext.jsx";
+import { useAuth } from "../contexts/AuthContext.jsx";
 
 function Logon() {
   const { login } = useAuth();
